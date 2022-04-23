5a65056afe54868488ffd745bd2390ad88cc1385
0xdoll <secdollg@gmail.com>
2022-04-23 21:17:45 +0800

    verify new contract

--- scripts/verify.js
+++ scripts/verify.js
@@ -1,14 +1,16 @@
-// 0x35D919A43F8A19FA3AA425a498C1a8a8A78c1979
 
 const hre = require("hardhat");
 
 async function main() {
 
-  var contract_addr = "0x7c8c43fbb3406380777fa92e4ded18c0718d4ab5";
+  var contract_addr = "0xc915c1E28f9CD37b9EF1376bF7bc805B01E71f28";
   await hre.run("verify:verify", {
     address: contract_addr,
     constructorArguments: [
-      3240,
+      "0xBFB9F99291cA3f040A1317ae835C7071Cf2432d0",
+      "0x7661F8e62658A42169f25DD6b3C58263726e9b4b",
+      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+      1,
     ],
   });
 }
